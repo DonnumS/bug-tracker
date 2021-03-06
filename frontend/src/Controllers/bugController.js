4d5bf1adf7d5819a8d3abaf98c61de394f263d1c
Sebastian Dønnum <sebbisen95@gmail.com>
2021-03-07 00:25:13 +0100

    ready to create login

---
+++ frontend/src/Controllers/bugController.js
@@ -0,0 +1,38 @@
+import bugModel from "../Models/bugModel";
+
+export function retrieveBugs() {
+  let data = [];
+
+  data.push(
+    new bugModel({
+      _id: 2345678,
+      name: "Crash on load",
+      details: "Crashes after 3 seconds",
+      steps: "Open application and it will crash",
+      version: "v2.0",
+      assigned: "Sebastian",
+      creator: "John Doe",
+      priority: 1,
+      time: "23:38",
+    })
+  );
+  data.push(
+    new bugModel({
+      _id: 2345678,
+      name: "Will not load",
+      details: "Nothing happens when opening application",
+      steps: "Open application",
+      version: "v2.0",
+      assigned: "Sebastian",
+      creator: "John Doe",
+      priority: 3,
+      time: "23:39",
+    })
+  );
+
+  let sorted = data.sort((a, b) => {
+    return a.priority - b.priority;
+  });
+
+  return sorted;
+}
