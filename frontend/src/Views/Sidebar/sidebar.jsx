2d1091a2d9e1bb6ed61ebaf4f45c1a96aee0b415
Sebastian Dønnum <sebbisen95@gmail.com>
2021-03-08 05:03:47 +0100

    color and style overhaul/tweaks

--- frontend/src/Views/Sidebar/sidebar.jsx
+++ frontend/src/Views/Sidebar/sidebar.jsx
@@ -21,22 +21,25 @@ export default () => {
       </Link>
       <ul>
         <li>
-          <Link to="/" className="nav-link">
+          <Link to="/" className="nav-link menu-item">
             Dashboard
           </Link>
         </li>
+        <hr className="gradient-hr"></hr>
         <li>
-          <Link to="/viewbugs" className="nav-link">
+          <Link to="/viewbugs" className="nav-link menu-item">
             View Bugs
           </Link>
         </li>
+        <hr className="gradient-hr"></hr>
         {auth.admin && (
           <li>
-            <Link to="/createbug" className="nav-link">
+            <Link to="/createbug" className="nav-link menu-item">
               Create Bug
             </Link>
           </li>
         )}
+        {auth.admin && <hr className="gradient-hr"></hr>}
       </ul>
       <button className="nav-link logout" onClick={SignOut}>
         Log Out
